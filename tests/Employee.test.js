8a778ea2436a15037c5ccbb66af6d83d109fc9d0
Aram Ambartsumyan <aram@Arams-MacBook-Air.local>
2022-10-27 00:15:52 -0700

    employee pass

--- tests/Employee.test.js
+++ tests/Employee.test.js
@@ -1,6 +1,48 @@
 const Employee = require('../lib/Employee.js')
 
-test('should create employee instance', () => {
+test('Should create employee instance', () => {
     const newEmployee = new Employee()
     expect(typeof newEmployee).toBe('object')
+});
+
+test('Should set name via constructor', () => {
+    const testName = 'Aram';
+    const newEmployee = new Employee(testName);
+    expect(newEmployee.name).toBe(testName);
+});
+
+test('Should set id via constructor', () => {
+    const testId = "2"
+    const newEmployee = new Employee('Aram', testId);
+    expect(newEmployee.id).toBe(testId);
+});
+
+test('Should set email via constructor', () => {
+    const testEmail = 'mail@test.com'
+    const newEmployee = new Employee('Aram', 2, testEmail);
+    expect(newEmployee.email).toBe(testEmail);
+});
+
+test('Should get name via getName()', () => {
+    const testName = 'Aram';
+    const newEmployee = new Employee(testName);
+    expect(newEmployee.getName()).toBe(testName);
+});
+
+test('Should get id via getId()', () => {
+    const testId = '2';
+    const newEmployee = new Employee('Aram', testId);
+    expect(newEmployee.getId()).toBe(testId);
+});
+
+test('Should get email via getEmail()', () => {
+    const testEmail = 'mail@test.com';
+    const newEmployee = new Employee('Aram', 2, testEmail);
+    expect(newEmployee.getEmail()).toBe(testEmail);
+});
+
+test('Should return Employee via getRole()', () => {
+    const testEmployee = 'Employee';
+    const newEmployee = new Employee('Aram', 2, 'mail@test.com');
+    expect(newEmployee.getRole()).toBe(testEmployee);
 });
